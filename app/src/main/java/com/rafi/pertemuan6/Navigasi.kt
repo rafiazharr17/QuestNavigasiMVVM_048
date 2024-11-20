bafe4d721c85b700d26891849b70b457a6af7f21
rafiazharr17 <rafipilo17@gmail.com>
2024-11-21 00:30:35 +0700

    membuat fungsi Navigasi beserta parameternya

--- app/src/main/java/com/rafi/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/rafi/pertemuan6/Navigasi.kt
@@ -1,6 +1,22 @@
 package com.rafi.pertemuan6
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.rafi.pertemuan6.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){
+
 }
